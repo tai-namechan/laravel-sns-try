6e6e88dc8c06c905b1655005d3e0d571a74069d7
Taisei Namekawa <s.namety.0106.hta1@gmail.com>
2021-05-24 00:01:12 +0900

    commit

--- config/services.php
+++ config/services.php
@@ -42,4 +42,10 @@ return [
         'client_secret' => env('FACEBOOK_SECRET'),
         'redirect'        => env('FACEBOOK_REDIRECT_URI'),
     ],
+
+    'github' => [
+        'client_id'        => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect'        => env('GITHUB_REDIRECT_URI'),
+    ],
 ];
