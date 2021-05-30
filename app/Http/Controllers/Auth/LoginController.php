e87c9f441367e048c7879a37be4dd4d55f3da4df
Taisei Namekawa <s.namety.0106.hta1@gmail.com>
2021-05-31 01:01:03 +0900

    aaaaa

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Hash;
 use Auth;
 use Socialite;
 use App\User;
